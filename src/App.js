c35a3877feed23275fd72c2192a5fe92ce308a4b
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-30 19:25:57 +0000

    removed dummy text and provided logo only

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+//<img src={logo} className="App-logo" alt="logo" />
 import './App.css';
+import './fonts/3A6A23_0_0.ttf'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"><h1 id="logo">Curatorial</h1></header>
+
+      
     </div>
   );
 }
