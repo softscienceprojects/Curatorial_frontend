347eb95768b68e16a0a10479823d3d7f0f460b2c
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-30 19:26:56 +0000

    started blocking

---
+++ src/components/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class SignUp extends React.Component {
+    state = {
+        email: '',
+        password: '',
+        password_confirmation: ''
+    }
+
+    return() {
+        <div>Sign Up Form</div>
+        
+    }
+}
