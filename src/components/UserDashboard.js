aa8a5cec898153f3d1fc3ec9b832bf1b8bc135d3
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-31 19:45:03 +0000

    generic blocking for personalisation

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import UserEditForm from './UserEditForm'
 
 class UserDashboard extends React.Component {
 
@@ -6,6 +7,7 @@ class UserDashboard extends React.Component {
         return(
             <div>
                 User DASHBOARD
+                hello,  
             </div>
         )
     }
