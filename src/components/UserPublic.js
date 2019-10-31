5c80a245e9e7cfa5395dac2866def32ee31d6ece
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-31 14:57:21 +0000

    user public generic blocking

---
+++ src/components/UserPublic.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class UserPublic extends React.Component {
+
+    render(){
+        return(
+            <div>
+                User Public
+            </div>
+        )
+    }
+
+}
+
+export default UserPublic
