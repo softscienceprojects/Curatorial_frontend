5da3bae175df4090b4a813160e5db552ba6e6600
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-31 14:57:35 +0000

    auth on users?

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,22 @@
 import React from 'react'
+//import API from '../adapters/API'
 
 class Users extends React.Component {
-
+    // state= {
+    //     user: null
+    // }
+    
+    // componentDidMount() {
+    //     API.validateUser().then(user=> {
+    //       if (user.errors) {
+    //         console.error(user.errors)
+    //         this.props.history.push('/signin')
+    //       } else {
+    //         this.setState({user})
+    //       }
+    //     })
+    // }
+ 
     render() {
         return(
             <div>USERS</div>
