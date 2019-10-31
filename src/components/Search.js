6eee8ad090fa880a55696fea793ec4813135a506
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-31 19:44:52 +0000

    Working search!

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,34 @@
 import React from 'react'
+import API from '../adapters/API'
 
 class Search extends React.Component {
-
     state={
-        searchTerm: ''
+        description: ''
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value 
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        API.search(this.state.description.toLowerCase())
+        .then(res=>console.log(res))
     }
 
     render() {
         return(
-            <div>This is the search form</div>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="description" placeholder="search for anything" value={this.state.description} onChange={this.handleChange} />
+                    <input type="submit" value="search" />
+                </form>
+            </div>
         )
     }
 }
 
 export default Search
+
