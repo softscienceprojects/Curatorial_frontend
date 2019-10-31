26e5d4812f745810733fbc222207e6c6f66a96fa
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-31 19:43:38 +0000

    generic artwork show page

---
+++ src/components/ArtworkShow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class ArtworkShow extends React.Component {
+
+    render() {
+        return(
+            <div>Artwork Show Page</div>
+        )
+    }
+}
+
+export default ArtworkShow
