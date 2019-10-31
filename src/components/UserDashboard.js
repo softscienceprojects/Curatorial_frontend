c59975af69dca4e11ab734973cc44358c88494e8
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-31 14:57:12 +0000

    dashboard generic handling

---
+++ src/components/UserDashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class UserDashboard extends React.Component {
+
+    render(){
+        return(
+            <div>
+                User DASHBOARD
+            </div>
+        )
+    }
+
+}
+
+export default UserDashboard
