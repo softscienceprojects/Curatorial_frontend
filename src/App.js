4268ed5431fde7a910ac77d5884b3648c9c423eb
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-31 14:55:52 +0000

    validation for user on app

--- src/App.js
+++ src/App.js
@@ -3,15 +3,67 @@ import React from 'react';
 //<img src={logo} className="App-logo" alt="logo" />
 import './App.css';
 import './fonts/3A6A23_0_0.ttf'
+import { Route } from 'react-router-dom'
+import { routes } from './config/routes'
+import NavBar from './components/NavBar'
+import API from './adapters/API';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"><h1 id="logo">Curatorial</h1></header>
+const notFoundMessage = () => <h5>NOT FOUND</h5>
 
-      
+class App extends React.Component {
+  state= {
+    user: null
+  }
+
+  signin = user => {
+    this.setState({user}, () => this.props.history.push('/users'))
+  }
+
+  logout = () => {
+    API.logout()
+    this.setState({user: null})
+    this.props.history.push('/signin')
+  }
+
+  componentDidMount() {
+    API.validateUser().then(user=> {
+      if (user.errors) {
+        console.error(user.errors)
+        this.props.history.push('/signin')
+      } else {
+        this.setState({user})
+      }
+    })
+}
+
+
+
+  render() {
+     return (
+      <div className="App">
+      <NavBar routes={routes} user={this.state.user} />
+        {routes.map(route => (
+          <Route
+            key={route.path}
+            exact
+            path={route.path}
+            component={routerProps =>
+              route.component ? (
+                <route.component
+                  {...routerProps}
+                  signin={this.signin}
+                  logout={this.logout}
+                />
+              ) : (
+                notFoundMessage()
+              )
+            }
+          />
+        ))}
     </div>
-  );
+      );
+  }
+ 
 }
 
 export default App;
