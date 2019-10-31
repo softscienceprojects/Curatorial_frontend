e0354d22599e384fb5d9b28fd92a50bc12bd2338
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-31 19:45:47 +0000

    removed users so does not show in navbar

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,6 @@
 import Home from "../components/Home";
 import SignIn from "../components/SignIn";
 import SignUp from "../components/SignUp";
-import Users from "../components/Users";
 import Search from "../components/Search";
 
 export const routes = [
@@ -19,11 +18,6 @@ export const routes = [
     title: "Sign up",
     path: "/signup",
     component: SignUp
-  },
-  {
-    title: "Users",
-    path: "/users",
-    component: Users
   },
   {
     title: "Log out",
