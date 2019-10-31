5a750ee39aeb71999a064f29d568ad8c9610c134
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-31 14:56:26 +0000

    signinform handles state

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import API from '../adapters/API'
 
 class SignIn extends React.Component {
+    state={
+        email: '',
+        password: ''
+    }
+
+    handleChange= (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        API.signin({email: this.state.email, password: this.state.password})
+        .then(user=> this.props.signin(user))
+    }
 
     render() {
         return(
-            <div>SignIn Form</div>
+            <div><h1>SignIn Form</h1>
+            <form onSubmit={this.handleSubmit}>
+                <input type="email" placeholder="email" name="email" value={this.state.email} onChange={this.handleChange} />
+                <input type="password" placeholder="password" name="password" value={this.state.password} onChange={this.handleChange} />
+                <input type="submit" value="Sign In" />
+            </form>
+            </div>
         )
     }
 }
