8e424808cdaec99041a5633aab1a9d37f6492aaf
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-31 15:38:59 +0000

    handling form submit to signup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,5 @@
 import React from 'react'
-//import API from '../adapters/API'
+import API from '../adapters/API'
 
 class SignUp extends React.Component {
     state = {
@@ -16,12 +16,14 @@ class SignUp extends React.Component {
 
     submitForm = event => {
         event.preventDefault()
+        API.signup({email: this.state.email, password: this.state.password, password_confirmation: this.state.password_confirmation})
+        .then(user=> this.props.signin(user))
     }
 
     render() {
         return(
            <div><h4>Sign UP</h4>
-           <form onSubmit={this.handleSubmit}>
+           <form onSubmit={this.submitForm}>
             <input type="email" placeholder="email" name="email" value={this.state.email} onChange={this.handleChange} />
             <input type="password" placeholder="password" name="password" value={this.state.password} onChange={this.handleChange} />
             <input type="password" placeholder="confirm your password" name="password_confirmation" value={this.state.password_confirmation} onChange={this.handleChange} />
