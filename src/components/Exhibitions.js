a0a035011b53240024a15062b430bde3ab18eff7
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-01 17:20:37 +0000

    exhibitions mounts in state

--- src/components/Exhibitions.js
+++ src/components/Exhibitions.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import API from '../adapters/API'
+import ExhibitionShow from './ExhibitionShow'
+import ExhibitionNewForm from './ExhibitionNewForm'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import PrivateRoute from '../config/privateroute'
 
 class Exhibitions extends React.Component {
+    state = {
+        exhibitions: []
+    }
+    componentDidMount() {
+        API.getExhibitions().then(response => 
+            this.setState( {exhibitions: response} )
+        )
+    }
 
     render() {
         return(
-        <div>This is exhibitions</div>
+        <Router>
+            <Switch>
+                <Route path="/">
+                    <ExhibitionShow />
+                </Route>
+                <PrivateRoute path="/new">
+                    <ExhibitionNewForm />
+                </PrivateRoute>
+            </Switch> 
+       </Router>
         )
     }
 }
