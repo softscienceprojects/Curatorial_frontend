6b472934b63e5343c34e272e314b67e15067b314
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-01 19:00:59 +0000

    exhibition show page renders content

--- src/components/ExhibitionShow.js
+++ src/components/ExhibitionShow.js
@@ -1,12 +1,32 @@
 import React from 'react'
-//import API from '../adapters/API'
+import API from '../adapters/API'
 
 class ExhibitionShow extends React.Component {
 
+    state = {
+        exhibition: null,
+        validating: true
+    }
+
+    componentDidMount() {
+        API.getExhibition(this.props.match.params.id).then(response=> this.setState({exhibition: response, validating: false}))
+    }
 
     render() {
+        if (this.state.validating) return <div className="loader">Curatorial</div>;
         return(
-        <div>EXHIBITION SHOW</div>
+        <div>
+            <h4>{this.state.exhibition.summary}</h4>
+
+            images here!
+            
+            <h2>{this.state.exhibition.title}</h2>
+            <h4>Curated by {this.state.exhibition.user.first_name}</h4>
+            <p>{this.state.exhibition.description}</p>
+
+            Like show stuff
+        
+        </div>
         )
     }
 }
