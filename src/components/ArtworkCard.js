d853ad2f1ca951d1e39dd229b2bfe9b46520e666
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-01 17:20:07 +0000

    Artwork card blocked

---
+++ src/components/ArtworkCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const ArtworkCard = (props) => {
+
+
+    return(
+    
+        <div>
+        <Link to={`explore/${props.artwork.id}`}>{props.artwork.title}</Link>
+        {/* {props.artwork.description.map(description=> <p>description</p>)} */}
+        
+        
+        </div>
+    )
+}
+
+export default ArtworkCard
