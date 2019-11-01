796dcc80b23c6fbb9a3881fded08b70d65de1a67
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-01 17:20:20 +0000

    mapping over artworks from props

--- src/components/Artworks.js
+++ src/components/Artworks.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import API from '../adapters/API'
+import ArtworkCard from './ArtworkCard'
+import { Link } from 'react-router-dom'
 
 class Artworks extends React.Component {
+    state={
+        artworks: []
+    }
+
+    componentDidMount() {
+        API.getArtworks().then(res=> this.setState({artworks: res}))
+    }
 
     render() {
         return(
-            <div>Artworks : Explore Page</div>
+            <div>Artworks : Explore Page
+                {this.state.artworks.map(artwork=> <ArtworkCard artwork={artwork}  />)}
+            </div>
         )
     }
 }
