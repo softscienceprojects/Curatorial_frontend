3e4b0461d72320a43da75a43027b535dddf7c08b
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-01 17:22:29 +0000

    auth for routes - now unused file

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,52 +1,99 @@
 import Home from "../components/Home";
-import Exhibitions from "../components/Exhibitions"
-import Artworks from "../components/Artworks"
+import Exhibitions from "../components/Exhibitions";
+import Artworks from "../components/Artworks";
+import Search from "../components/Search";
+
 import SignIn from "../components/SignIn";
 import SignUp from "../components/SignUp";
-import Search from "../components/Search";
 import Users from "../components/Users";
 
-export const routes = [
+const routes = [
   {
     title: "Home",
     path: "/",
-    component: Home
+    component: Home,
+    auth: false
   },
   {
     title: "Exhibitions",
     path: "/exhibitions",
-    component: Exhibitions
+    component: Exhibitions,
+    auth: false
   },
   {
     title: "Explore",
     path: "/explore",
-    component: Artworks
+    component: Artworks,
+    auth: false
   },
   {
     title: "Search",
     path: "/search",
-    component: Search
-  },
-  {
-    title: "Sign in",
-    path: "/signin",
-    component: SignIn
-  },
-  {
-    title: "Sign up",
-    path: "/signup",
-    component: SignUp
+    component: Search,
+    auth: false
   },
   {
     title: "Dashboard",
-    path: `/users/`,
-    component: Users
+    path: "/users",
+    component: Users,
+    auth: true
   },
   {
     title: "Log out",
     path: "/logout",
+    auth: true,
     component: props => {
       props.logout();
-      return null;}
+      return null;
+    }
+  },
+  {
+    title: "Sign in",
+    path: "/signin",
+    component: SignIn,
+    auth: false,
+    hideAuth: true
+  },
+  {
+    title: "Sign up",
+    path: "/signup",
+    component: SignUp,
+    auth: false,
+    hideAuth: true
   }
 ];
+
+// const loggedInRoutes = [
+//   {
+//     title: "Dashboard",
+//     path: "/users",
+//     component: Users,
+//     auth: true
+//   },
+//   {
+//     title: "Log out",
+//     path: "/logout",
+//     auth: true,
+//     component: props => {
+//       props.logout();
+//       return null;
+//     }
+//   }
+// ];
+
+// const loggedOutRoutes = [
+//   {
+//     title: "Sign in",
+//     path: "/signin",
+//     component: SignIn,
+//     auth: false
+//   },
+//   {
+//     title: "Sign up",
+//     path: "/signup",
+//     component: SignUp,
+//     auth: false
+//   }
+// ];
+
+export { routes };
