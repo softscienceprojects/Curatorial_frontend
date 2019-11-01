e3fd86a6f4e8c0343a6344c233513a4a5eb5375f
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-01 17:22:14 +0000

    paths for navbar

---
+++ src/config/paths.js
@@ -0,0 +1,10 @@
+export default {
+  SIGNIN: "/signin",
+  LOGOUT: "/logout",
+  SIGNUP: "/signup",
+  ROOT: "/",
+  EXHIBITIONS: "/exhibitions",
+  EXPLORE: "/explore",
+  SEARCH: "/search",
+  USERS: "/users" 
+};
