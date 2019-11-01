a183041b56618b566dc306ca11ec995dd4ecbe3d
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-01 19:33:53 +0000

    userDashboard validating

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -2,18 +2,30 @@ import React from 'react'
 //import UserEditForm from './UserEditForm'
 
 class UserDashboard extends React.Component {
-
-    componentDidMount() {
-        console.log("user dashboard mounted")
+    state = {
+        validating: true
     }
 
+    componentDidMount() {
+        console.log(this.props.user)
+        // this.setState({
+        //     validating: false
+        // })
+    }
 
+    componentDidUpdate() {
+        this.setState({
+            validating: false
+        })  
+    }
 
     render(){
+        if (this.state.validating) return <div className="loader">Curatorial</div>;
         return(
             <div>
                 User DASHBOARD
                 hello
+                 {this.props.user.first_name}
             </div>
         )
     }
