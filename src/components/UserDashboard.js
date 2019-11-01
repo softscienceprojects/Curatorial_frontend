0a8f1327ede5872ceffbdd1edfdbc66b30875b2c
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-01 20:52:45 +0000

    handle render with null return

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -2,30 +2,19 @@ import React from 'react'
 //import UserEditForm from './UserEditForm'
 
 class UserDashboard extends React.Component {
-    state = {
-        validating: true
-    }
-
-    componentDidMount() {
-        console.log(this.props.user)
-        // this.setState({
-        //     validating: false
-        // })
-    }
-
-    componentDidUpdate() {
-        this.setState({
-            validating: false
-        })  
-    }
 
     render(){
-        if (this.state.validating) return <div className="loader">Curatorial</div>;
+        if (this.props.user === null) return null; // loading, not auth'd
+
+        // if this.props.match.params.id === this.props.user.id (this is you)
         return(
             <div>
                 User DASHBOARD
-                hello
-                 {this.props.user.first_name}
+               <h4> {this.props.user.first_name}</h4>
+                <p>followers/following</p>
+
+                <p>{this.props.user.biography}</p>
+
             </div>
         )
     }
