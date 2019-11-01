bcd418a1a9f10a2bd9a808eb0d88704a4de41d58
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-01 17:21:17 +0000

    swap to hardcoded navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 
-const NavBar = ({routes, user}) => {
-    return (
-        <nav>
-            {routes.map(route=> (<Link key={route.path} to={route.path}>{route.title}</Link>))}
-            NAVBAR: {user ? <p>{user.first_name}</p> : null}
-        </nav>
-    )
-}
+const NavBar = ({ user }) => {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/exhibitions">Exhibitions</Link>
+      <Link to="/explore">Explore</Link>
+      <Link to="/search">Search</Link>
+      {user && <Link to="/users">Dashboard</Link>}
+      {user && <Link to="/logout">Log out</Link>}
+      {!user && <Link to="/signin">Sign in</Link>}
+      {!user && <Link to="/signup">Sign up</Link>}
+      {user ? <p>{user.first_name}</p> : null}
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
+
+// {
+//   title: "Log out",
+//   path: "/logout",
+//   auth: true,
+//   component: props => {
+//     props.logout();
+//     return null;
+//   }
