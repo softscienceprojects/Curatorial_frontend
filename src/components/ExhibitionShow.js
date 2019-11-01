dbabfbfbdb25ec220d223b8fd506949c639b9062
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-01 17:21:06 +0000

    Exhibition show blocked

---
+++ src/components/ExhibitionShow.js
@@ -0,0 +1,15 @@
+import React from 'react'
+//import API from '../adapters/API'
+
+class ExhibitionShow extends React.Component {
+
+
+    render() {
+        return(
+        <div>EXHIBITION SHOW</div>
+        )
+    }
+}
+
+export default ExhibitionShow
+
