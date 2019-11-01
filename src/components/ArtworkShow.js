d20575300e210c7ca7b35227cc1d9fedda1ca927
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-01 17:54:28 +0000

    artwork now rendering

--- src/components/ArtworkShow.js
+++ src/components/ArtworkShow.js
@@ -2,10 +2,27 @@ import React from 'react'
 import API from '../adapters/API'
 
 class ArtworkShow extends React.Component {
+    state ={
+        artwork: null,
+        validating: true
+    }
     
+    componentDidMount() {
+        API.getArtwork(this.props.match.params.id).then(response=> this.setState({artwork: response, validating: false}))
+    }
+
+
     render() {
+        if (this.state.validating) return <div className="loader">Curatorial</div>;
         return(
-            <div>Artwork Show Page </div>
+            <div>
+                Image will go here
+                <h2>{this.state.artwork.title}</h2>
+                <h4>by {this.state.artwork.artist}</h4>
+                <p>{this.state.artwork.medium}</p>
+                <p>{this.state.artwork.description}</p>
+                <p>{this.state.artwork.collection} - {this.state.artwork.location}</p>
+            </div>
 
         )
     }
