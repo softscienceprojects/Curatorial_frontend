53d2613b34621d535cf146c1a884bc744c493297
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-02 16:59:45 +0000

    removed not in use

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,13 +1,10 @@
 import React from 'react'
 //import API from '../adapters/API'
-import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
-import UserDashboard from "./UserDashboard"
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import UserEditForm from "./UserEditForm"
 import ExhibitionNewForm from './ExhibitionNewForm'
 
 export default class Users extends React.Component {
-
-    
 
     render() {
         return(
