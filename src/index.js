6e48bf63b942adf9f8c8793418834040bf635690
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-02 13:21:12 +0000

    updated browser router to router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Router, Route } from 'react-router-dom'
 import { createBrowserHistory } from "history"
 
 const curatorialHistory = createBrowserHistory();
