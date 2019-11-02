3518077c175354142d0e007dc4f42423fa95abea
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-02 14:09:41 +0000

    handling flow of events for search terms

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -4,7 +4,8 @@ import ArtworkCard from "./ArtworkCard"
 class SearchResults extends React.Component {
     
     render() {
-        if (this.props.results === []) return <div>There's nothing here</div>;
+        if (!this.props.results) return <div><h1>Search for anything</h1></div>
+        if (this.props.results.length === 0) return <div><h1>You're an original</h1> <h4>Nothing matches that search</h4></div>;
         return(
             <div>
                 {this.props.results.map(artwork=> <ArtworkCard key={artwork.id} artwork={artwork}/> )}
