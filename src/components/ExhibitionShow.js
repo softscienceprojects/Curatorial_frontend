f091d7984ef852c3c1cbb7cc9e34399367768e8a
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-04 17:11:29 +0000

    removed comments and added remove button

--- src/components/ExhibitionShow.js
+++ src/components/ExhibitionShow.js
@@ -16,37 +16,20 @@ class ExhibitionShow extends React.Component {
         API.getExhibition(this.props.match.params.id).then(response=> this.setState({exhibition: response, validating: false}))
     }
 
-    addALike = (response) => {
-        this.setState({
-            exhibition: {
-                ...this.state.exhibition,
-                exhibition_likes: [...this.state.exhibition.exhibition_likes, response]
-            }
-        })
-    }
-
-    removeALike = (response) => {
-       let toRemove = this.state.exhibition.exhibition_likes.find(like=> like.id === response.id)
-       this.setState({
-            exhibition: {
-                ...this.state.exhibition,
-                exhibition_likes: this.state.exhibition.exhibition_likes.filter(like => like !== toRemove)
-            }
-        })
-    }
+  
 
     render() {
         if (this.state.validating) return <LoadingComponent />;
         //if (this.state.validating) return <div className="loader">Curatorial</div>;
         return(
         <div>
-            { this.props.user.id === this.state.exhibition.user.id 
+            { this.props.user && this.props.user.id === this.state.exhibition.user.id 
                 ? <Link to={{pathname: `/exhibitions/${this.state.exhibition.id}/edit`}} >Edit this exhibition</Link> 
                 : null }
 
             <h4>{this.state.exhibition.summary}</h4>
 
-            {this.state.exhibition.artworks.length > 0 ? this.state.exhibition.artworks.map(artwork => <ArtworkCard key={artwork.title} artwork={artwork}/>) : "Currently no artworks"}
+            {this.state.exhibition.artworks.length > 0 ? this.state.exhibition.artworks.map(artwork => <><ArtworkCard key={artwork.title} artwork={artwork} /><RemoveArtworkButton user={this.props.user} artwork={artwork.id} exhibition={this.state.exhibition}/></> ) : "Currently no artworks"}
             
             
             <h2>{this.state.exhibition.title}</h2>
@@ -57,11 +40,27 @@ class ExhibitionShow extends React.Component {
             <h4>{this.state.exhibition.exhibition_likes.length} {this.state.exhibition.exhibition_likes.length === 0 || this.state.exhibition.exhibition_likes.length > 1 ? "Curators like this exhibition" : "Curator likes this exhibition"}</h4>
 
             {this.props.user ? <LikeUnlikeExhibition user={this.props.user} 
-            exhibition_id={this.state.exhibition.id} exhibition={this.state.exhibition} addALike={this.addALike} removeALike={this.removeALike} /> : "Loading..."}
+            exhibition_id={this.state.exhibition.id} exhibition={this.state.exhibition} 
+            addALike={this.props.userAddExhibitionLike} 
+            // addALike={this.addALike} 
+            removeALike={this.props.userRemoveExhibitionLike} 
+            // removeALike={this.removeALike} 
+            /> : null}
         </div>
         )
     }
 }
 
 export default ExhibitionShow
+
+const RemoveArtworkButton = (props) => {
+    if (props.user && props.user.id === props.exhibition.user.id) {
+       return(
+        <button >Remove button</button>
+     ) 
+    } else {
+        return null
+    }
+    // props.user.id === props.exhibition.user.id
+}
 
