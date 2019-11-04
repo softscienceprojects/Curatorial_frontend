68b4466d8d22781ceeab4fc248e197229c98aa8e
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-04 14:33:28 +0000

    image and detail on artwork card

--- src/components/ArtworkCard.js
+++ src/components/ArtworkCard.js
@@ -7,10 +7,11 @@ const ArtworkCard = (props) => {
     return(
     
         <div>
+        <img src={props.artwork.image_url} alt={props.artwork.title}  className="responsive" />
         <Link to={{pathname: `/explore/${props.artwork.id}`}} key={props.artwork.id}>{props.artwork.title}</Link>
         {/* {props.artwork.description.map(description=> <p>description</p>)} */}
-        
-        
+        <p>By {props.artwork.artist}</p>
+        <p>{props.artwork.medium}</p>
         </div>
     )
 }
