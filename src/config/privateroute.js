4ae47e03673026e93dc0d1d201bf4056a8cce201
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-05 17:55:55 +0000

    private route prep

--- src/config/privateroute.js
+++ src/config/privateroute.js
@@ -1,25 +1,48 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import curatorialAuth from './Authenticated'
+import API from '../adapters/API'
 
-function PrivateRoute({ children, ...rest }) {
-    return (
-      <Route
-        {...rest}
-        render={({ location }) =>
-            curatorialAuth.isAuthenticated ? (
-            children
-          ) : (
-            <Redirect
-              to={{
-                pathname: "/login",
-                state: { from: location }
-              }}
-            />
-          )
-        }
-      />
-    );
+
+
+class PrivateRoute extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidMount() {
+    API.validateUser().then(user => {
+      this.setState({ validating: false });
+      if (user.errors) {
+        //console.error(user.errors);
+        //this.props.history.push("/signin");
+      } else {
+        this.setState({ user });
+      }
+    });
+  } 
+
+    render() {
+          return (
+        <Route
+          {...rest}
+          render={({ location }) =>
+              curatorialAuth.isAuthenticated ? (
+              children
+            ) : (
+              <Redirect
+                to={{
+                  pathname: "/login",
+                  state: { from: location }
+                }}
+              />
+            )
+          }
+        />
+      );
+    }
+
   }
 
   export default PrivateRoute
