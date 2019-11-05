ddedf9277b283bd28f988f1de2c739cfa476925c
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-05 11:23:57 +0000

    link to user edit account

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-//import UserEditForm from './UserEditForm'
+
 import ExhibitionCard from './ExhibitionCard';
 
 
@@ -20,36 +20,26 @@ class UserDashboard extends React.Component {
     }
 
 
-
     render(){
         if (this.props.user === null) return null; // loading, not auth'd
         // this.props.history.push("/signin");
         // if this.props.match.params.id === this.props.user.id (this is you)
         return(
             <div>
-                User DASHBOARD
+                <h4>Welcome, {this.props.user.first_name}</h4>
                 <br />
-                {JSON.stringify(this.props.match.params.id) === JSON.stringify(this.props.user.id) ? userDashboardOptions(this.props.user) : null }
-
-                {/* User Edit Form, comment back in if ready to finish: */}
-                {/* <UserEditForm user={this.props.user} /> */}
-
-               <h4> {this.props.user.first_name}</h4>
+                {parseInt(this.props.match.params.id) === this.props.user.id ? <UserDashboardOptions user={this.props.user} /> : null }              
                 {/* <p>followers/following</p> */}
-
                 <p>{this.props.user.biography}</p>
 
-                {/* Exhibition likes currently doesn't have link to exhibition from user */}
                 <h4>liked shows</h4>
                 {this.props.user.exhibition_likes.map(liked=> <ExhibitionCard exhibition={liked.exhibition} key={liked.exhibition.id} /> )}
 
                 <h4>curated shows</h4>
                 {this.props.user.exhibitions.map(exhibition=> <ExhibitionCard exhibition={exhibition} key={exhibition.id} /> )}
                 
-                {/* If followers/following
-                <h4>feed</h4> */}
+                {/* <h4>feed</h4>  */}
 
-                
             </div>
         )
     }
@@ -59,11 +49,11 @@ export default UserDashboard
 
 // If it's you who is logged in:
 
-const userDashboardOptions = (props) => {
+const UserDashboardOptions = (props) => {
     
     return(
         <nav>
-            <Link to={{pathname: `/users/${props.id}/edit`}}>Edit your account</Link>
+            <Link to={{pathname: `/users/${props.user.id}/edit`}}>Edit your account</Link>
         </nav>
     )
 }
