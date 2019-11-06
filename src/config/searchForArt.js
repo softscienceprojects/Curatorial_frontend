5401957583bb97e425579e12fcf455d825ef01b6
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-06 18:58:37 +0000

    removed comment

--- src/config/searchForArt.js
+++ src/config/searchForArt.js
@@ -3,7 +3,6 @@ import API from '../adapters/API'
 
 const searchForArt = (searchTerm, props) => {
     API.search(searchTerm.toLowerCase())
-    //.then(results => console.log(props))
    .then( results => props.history.push(`/results/description=${searchTerm}`, {...results})
     ).catch(error=> error)
 }
