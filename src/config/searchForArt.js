dbfdeb7cdfb7548b2368a0c6817ffd24579cb4c5
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-06 19:35:23 +0000

    removed unused comment

--- src/config/searchForArt.js
+++ src/config/searchForArt.js
@@ -2,13 +2,6 @@ import React from 'react'
 import API from '../adapters/API'
 
 const searchForArt = (searchTerm, props) => {
-    // if (this.props.location.search) {
-    //     let term = this.props.location.search.split("=")[1].replace(/%20/gi, " ")
-    //        this.setState({
-    //        searchTerm: term
-    //         }) 
-    //     }
-    console.log(props)
     API.search(searchTerm.toLowerCase())
    .then( results => props.history.push(`/results/description=${searchTerm}`, {...results})
     ).catch(error=> error)
