77a045c1527f32789c68f6c8bf0b1de47c0b97df
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-06 13:29:47 +0000

    react-video installed

--- src/components/HomePanel2.js
+++ src/components/HomePanel2.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { Player,ControlBar } from 'video-react'
 
 const HomePanel2 = props => {
   return (<div>
     (middle panel - full image background)
+    <Player autoPlay src="https://media.giphy.com/media/3rgXBBs5EAc8BIMD7i/giphy.mp4">
+    <ControlBar display="none" autoHide={true} />
+    </Player>
     <h4>Discover shows curated by other users</h4>
   </div>
   );
