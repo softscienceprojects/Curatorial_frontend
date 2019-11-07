8282de4b32e9dab5b5a8277487a546ea98ecaf96
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-07 10:45:30 +0000

    added video

--- src/components/HomePanel2.js
+++ src/components/HomePanel2.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { Player,ControlBar } from 'video-react'
+//import { Player,ControlBar } from 'video-react'
+import ReactPlayer from 'react-player'
 
 const HomePanel2 = props => {
-  return (<div>
+  return (
+  <div className="home-panels">
     (middle panel - full image background)
-    <Player autoPlay src="https://media.giphy.com/media/3rgXBBs5EAc8BIMD7i/giphy.mp4">
-    <ControlBar display="none" autoHide={true} />
-    </Player>
+    {/* <Player playing={true} autoPlay={true} loop={true} width="100%" src="https://media.giphy.com/media/3rgXBBs5EAc8BIMD7i/giphy.mp4">
+    <ControlBar autoHide={true} display="none" />
+    </Player> */}
+    <ReactPlayer url="https://media.giphy.com/media/3rgXBBs5EAc8BIMD7i/giphy.mp4" playing loop="true" width="100vw" />
     <h4>Discover shows curated by other users</h4>
   </div>
   );
