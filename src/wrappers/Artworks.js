ee704800f4f649ae733c8b399592c09dda14c3e0
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-07 15:02:11 +0000

    infinite scroll prepped

--- src/wrappers/Artworks.js
+++ src/wrappers/Artworks.js
@@ -3,6 +3,8 @@ import API from '../adapters/API'
 import ArtworkCard from '../components/ArtworkCard'
 //import { Link } from 'react-router-dom'
 
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 import LoadingComponent from "../components/LoadingComponent"
 
 class Artworks extends React.Component {
@@ -12,15 +14,22 @@ class Artworks extends React.Component {
     }
 
     componentDidMount() {
-        API.getArtworks().then(res=> this.setState({artworks: res, loading: false}))
+        API.getArtworksCapped().then(res=> this.setState({artworks: res, loading: false}))
+    }
+
+    loader = () => {
+        API.getArtworksCapped().then(res=> this.setState({
+            artworks: this.state.artworks.concat(Array.from([...res])), 
+            loading: false}))
     }
 
     render() {
         if (this.state.loading) return <LoadingComponent />
         return(
             <>
-            
+            {/* <InfiniteScroll dataLength={this.state.artworks.length} next={this.loader} refreshFunction={this.loader} hasMore={true} pullDownToRefresh loader={<h1 id="logo">Getting art</h1>}> */}
                 {this.state.artworks.map(artwork=> <ArtworkCard artwork={artwork} key={artwork.id} />)}
+            {/* </InfiniteScroll> */}
             </>
         )
     }
