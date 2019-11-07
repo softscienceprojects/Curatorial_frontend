d6be0f15271a153d90ec6f1edd504ddc608a620d
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-07 15:47:00 +0000

    styling tags on search results / explore

--- src/components/ArtworkCard.js
+++ src/components/ArtworkCard.js
@@ -7,15 +7,19 @@ const ArtworkCard = (props) => {
     return(
     
         <div className="artwork-card">
-        {/* <img src={props.artwork.image_url} alt={props.artwork.title}  className="responsive" /> */}
-        <Link to={{pathname: `/explore/${props.artwork.id}`}} key={props.artwork.id}>{props.artwork.title}</Link>
+        {/* <Link to={{pathname: `/explore/${props.artwork.id}`}} key={props.artwork.id}><img src={props.artwork.image_url} alt={props.artwork.title}  className="responsive" /></Link> */}
         
         {/* <span className="tag" key={content.description}><button onClick={()=> searchforArt(content.description, this.props)}>{content.description}</button></span> */}
-            {/* {props.artwork.contents.map(content=> 
-            content.description !== props.match.params.id 
+            {props.searchTerm ? 
+            props.artwork.contents.map(content=> 
+            content.description !== props.searchTerm
             ? <span className="tag">&nbsp;{content.description}&nbsp;</span>
-            : <span className="tag-selected">&nbsp;{content.description}&nbsp;</span> )} */}
-
+            : <span className="tag-selected">&nbsp;{content.description}&nbsp;</span> 
+            ) :
+            props.artwork.contents.map(content=>  
+            <span className="tag">&nbsp;{content.description}&nbsp;</span>
+            )}  
+        <h4><Link to={{pathname: `/explore/${props.artwork.id}`}} key={props.artwork.id}>{props.artwork.title}</Link></h4>
         <p>By {props.artwork.artist}</p>
         <p>{props.artwork.medium}</p>
         </div>
--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -9,9 +9,10 @@ class SearchResults extends React.Component {
     render() {
         return(
             <article>
+                
                 {!this.props.location ? <LoadingComponent /> : null }
                 {this.props.location && this.props.location.state.errors ? <NoResults searchTerm={this.props.match.params.id} /> : null }
-                {this.props.location && this.props.location.state.content ? this.props.location.state.content.artworks.map(artwork=> <ArtworkCard {...this.props} key={artwork.id} artwork={artwork}/> ) : null }
+                {this.props.location && this.props.location.state.content ? this.props.location.state.content.artworks.map(artwork=> <ArtworkCard {...this.props} searchTerm={this.props.match.params.id} key={artwork.id} artwork={artwork}/> ) : null }
             </article>
         )
     }
