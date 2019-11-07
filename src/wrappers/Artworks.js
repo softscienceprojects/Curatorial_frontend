2fb47b73b755987839b3d643631811d34a03618b
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-07 10:45:52 +0000

    keep track of loading

--- src/wrappers/Artworks.js
+++ src/wrappers/Artworks.js
@@ -4,21 +4,24 @@ import ArtworkCard from '../components/ArtworkCard'
 //import { Link } from 'react-router-dom'
 import NavBar from "../components/NavBar"
 import Footer from "../components/Footer"
+import LoadingComponent from "../components/LoadingComponent"
 
 class Artworks extends React.Component {
     state={
-        artworks: []
+        artworks: [], 
+        loading: true
     }
 
     componentDidMount() {
-        API.getArtworks().then(res=> this.setState({artworks: res}))
+        API.getArtworks().then(res=> this.setState({artworks: res, loading: false}))
     }
 
     render() {
+        if (this.state.loading) return <LoadingComponent />
         return(
             <>
             <NavBar user={this.props.user} />
-            Artworks : Explore Page
+            
                 {this.state.artworks.map(artwork=> <ArtworkCard artwork={artwork} key={artwork.id} />)}
             <Footer />
             </>
