a381fa67111e9a9f09273615a315bac5f09b0f6f
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-07 15:26:30 +0000

    navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,13 +6,13 @@ const NavBar = ({ user }) => {
   return (
     <nav>
       <h1 id="logo">Curatorial</h1>
-      <NavLink to="/">Home</NavLink>&nbsp;
-      <NavLink to="/explore">Explore</NavLink>&nbsp;
-      <NavLink to="/search">Search</NavLink>&nbsp;
-      {user && <NavLink to={`/users/${user.id}`}>Dashboard</NavLink>}&nbsp;
-      {user && <NavLink to="/logout">Log out</NavLink>}&nbsp;
-      {!user && <NavLink to="/signin">Sign in</NavLink>}&nbsp;
-      {!user && <NavLink to="/signup">Sign up</NavLink>}
+      <NavLink className="nav-link" to="/">Home</NavLink>&nbsp;
+      <NavLink className="nav-link"to="/explore">Explore</NavLink>&nbsp;
+      <NavLink className="nav-link" to="/search">Search</NavLink>&nbsp;
+      {user && <NavLink className="nav-link" to={`/users/${user.id}`}>Dashboard</NavLink>}&nbsp;
+      {user && <NavLink className="nav-link" to="/logout">Log out</NavLink>}&nbsp;
+      {!user && <NavLink className="nav-link" to="/signin">Sign in</NavLink>}&nbsp;
+      {!user && <NavLink className="nav-link" to="/signup">Sign up</NavLink>}
     </nav>
   );
 };
