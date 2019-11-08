b14a753bd4b0685d410682050a0f6a458c3c0a0f
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-08 18:31:52 +0000

    keep image commented out

--- src/components/ArtworkCard.js
+++ src/components/ArtworkCard.js
@@ -7,6 +7,7 @@ const ArtworkCard = (props) => {
     return(
     
         <div className="artwork-card">
+        {/* Image below:  */}
         {/* <Link to={{pathname: `/explore/${props.artwork.id}`}} key={props.artwork.id}><img src={props.artwork.image_url} alt={props.artwork.title}  className="responsive" /></Link> */}
         
         {/* <span className="tag" key={content.description}><button onClick={()=> searchforArt(content.description, this.props)}>{content.description}</button></span> */}
