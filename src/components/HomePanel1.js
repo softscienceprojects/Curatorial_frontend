5777f2f3b28336e83263f628f03de58b2d2fe361
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-09 17:24:44 +0000

    remove name (showing in navbar)

--- src/components/HomePanel1.js
+++ src/components/HomePanel1.js
@@ -3,7 +3,6 @@ import React from "react";
 const HomePanel1 = props => {
   return <div className="home-panels home-panel-1">
     <div className="hero">
-      <h1 id="logo">Curatorial</h1>
     <h4>Explore the world of art or curate your own</h4>
     </div>
 
