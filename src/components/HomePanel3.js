d5a668f6b3dc8a97b04bc5f0f0720884a3676f3d
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-10 14:54:46 +0000

    added text to hero div

--- src/components/HomePanel3.js
+++ src/components/HomePanel3.js
@@ -3,7 +3,9 @@ import React from "react";
 const HomePanel3 = props => {
   return (
     <div className="home-panels home-panel-3">
+      <div className="hero">
         <h1 id="logo">Powered by the Havard Art Museums and Google Vision APIs</h1>
+      </div>
     </div>
     )
 };
