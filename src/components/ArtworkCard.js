450c2f12dbf4d31af76df5d83a8a7a3db918572a
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-10 12:56:30 +0000

    art-tags have div to control overflow

--- src/components/ArtworkCard.js
+++ src/components/ArtworkCard.js
@@ -11,6 +11,7 @@ const ArtworkCard = (props) => {
         {/* Image below:  */}
         {/* <Link to={{pathname: `/explore/${props.artwork.id}`}} key={props.artwork.id}><img src={props.artwork.image_url} alt={props.artwork.title}  className="responsive" /></Link> */}
         
+        <div className="art-tags">
         {/* <span className="tag" key={content.description}><button onClick={()=> searchforArt(content.description, this.props)}>{content.description}</button></span> */}
             {props.searchTerm ? 
             props.artwork.contents.map(content=> 
@@ -21,6 +22,8 @@ const ArtworkCard = (props) => {
             props.artwork.contents.map(content=>  
             <label className="tag">&nbsp;{content.description}&nbsp;</label>  
             )}  
+        </div>
+
         <h4><Link to={{pathname: `/explore/${props.artwork.id}`}} key={props.artwork.id}>{props.artwork.title}</Link></h4>
         <p>By {props.artwork.artist}</p>
         <p>{props.artwork.medium}</p>
