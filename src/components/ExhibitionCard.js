f1bf79e1a32d970f5e99b3a909fac5b7827a5100
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-10 15:58:03 +0000

    exhibition name in logo font

--- src/components/ExhibitionCard.js
+++ src/components/ExhibitionCard.js
@@ -7,7 +7,7 @@ const ExhibitionCard = (props) => {
     return(
     
         <div className="exhibition-card">
-        <h5><Link to={{pathname: `/exhibitions/${props.exhibition.id}`}}>{props.exhibition.title}</Link></h5>
+        <h2 id="logo"><Link to={{pathname: `/exhibitions/${props.exhibition.id}`}}>{props.exhibition.title}</Link></h2>
         <h6>{props.exhibition.summary}</h6>
         </div>
     )
