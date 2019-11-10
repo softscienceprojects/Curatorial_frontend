f4867c0f7a9da8db14d5208a81a43495ad647ba7
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-10 14:09:55 +0000

    returned h1 to nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,16 +5,19 @@ import { NavLink, Link } from "react-router-dom";
 const NavBar = ({ user }) => {
   //console.log(user === null)
   return (
-    <nav id="home-logo">
-      <Link to="/"><h1 id="nav-logo">Curatorial</h1></Link>&nbsp;&nbsp;
-      <nav>
+    <>
+    <nav>
+      <Link to="/"><h1 id="nav-logo">Curatorial</h1></Link>
+    
+      
       <NavLink className="nav-link"to="/explore">Explore</NavLink>&nbsp;
       <NavLink className="nav-link" to="/search">Search</NavLink>&nbsp;
       {user && <NavLink className="nav-link" to={`/users/${user.id}`}>Dashboard</NavLink>}&nbsp;
       {user && <NavLink className="nav-link" to="/logout">Log out</NavLink>}&nbsp;
       {!user && <NavLink className="nav-link" to="/signin">Sign in</NavLink>}&nbsp;
       {!user && <NavLink className="nav-link" to="/signup">Sign up</NavLink>}
-    </nav></nav>
+    </nav>
+    </>
   );
 };
 
