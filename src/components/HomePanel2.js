b3fb5b5835197f373fa8f16ba832a9be6b8bbb84
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-10 18:02:07 +0000

    removed video background and swapped for image

--- src/components/HomePanel2.js
+++ src/components/HomePanel2.js
@@ -4,12 +4,11 @@ import ReactPlayer from 'react-player'
 
 const HomePanel2 = props => {
   return (
-  <div className="home-panels">
-    {/* <Player playing={true} autoPlay={true} loop={true} width="100%" src="https://media.giphy.com/media/3rgXBBs5EAc8BIMD7i/giphy.mp4">
-    <ControlBar autoHide={true} display="none" />
-    </Player> */}
-    <ReactPlayer url="https://media.giphy.com/media/3rgXBBs5EAc8BIMD7i/giphy.mp4" playing loop={true} width="100vw" />
+  <div className="home-panels home-panel-2">
+    <div className="hero">
+    {/* <ReactPlayer url="https://media.giphy.com/media/3rgXBBs5EAc8BIMD7i/giphy.mp4" playing loop={true} width="100vw" /> */}
     <h4>Discover shows curated by other users</h4>
+  </div>
   </div>
   );
 };
