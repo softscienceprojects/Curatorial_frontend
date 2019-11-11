2bb91361e0ee04661124c7e05943ff5a3a6ec998
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-11 20:02:14 +0000

    update h4 to h1

--- src/components/HomePanel1.js
+++ src/components/HomePanel1.js
@@ -3,7 +3,7 @@ import React from "react";
 const HomePanel1 = props => {
   return <div className="home-panels home-panel-1">
     <div className="hero">
-    <h4>Explore the world of art or curate your own</h4>
+    <h1 id="logo">Explore the world of art or curate your own</h1>
     </div>
 
   </div>;
--- src/components/HomePanel2.js
+++ src/components/HomePanel2.js
@@ -7,7 +7,7 @@ const HomePanel2 = props => {
   <div className="home-panels home-panel-2">
     <div className="hero">
     {/* <ReactPlayer url="https://media.giphy.com/media/3rgXBBs5EAc8BIMD7i/giphy.mp4" playing loop={true} width="100vw" /> */}
-    <h4>Discover shows curated by other users</h4>
+    <h1 id="logo">Discover shows curated by other users</h1>
   </div>
   </div>
   );
