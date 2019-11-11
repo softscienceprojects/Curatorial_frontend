40874e19ce0a86219367e28631375c89703bf69e
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-11 17:14:12 +0000

    buggy on user show page, removed conditonal style

--- src/components/ExhibitionCard.js
+++ src/components/ExhibitionCard.js
@@ -2,11 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const ExhibitionCard = (props) => {
-    let whiteCube = props.exhibition.artworks.length === 0
 
     return(
        
-        <div className="exhibition-card" style={{ backgroundImage: whiteCube ? null : `url(${props.exhibition.artworks[0].image_url})` }} >
+        <div className="exhibition-card" >
         <h2 id="logo"><Link to={{pathname: `/exhibitions/${props.exhibition.id}`}}>{props.exhibition.title}</Link></h2>
         <h6>{props.exhibition.summary}</h6>
          </div>
