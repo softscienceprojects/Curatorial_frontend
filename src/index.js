b4a3720f8712277ec8d3fa4d23b4c7e35620b710
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-12 11:31:04 +0000

    scroll_to removed

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Router, Route } from 'react-router-dom'
-import { createBrowserHistory } from "history"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Router, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import ErrorBoundary from "./components/ErrorBoundary";
 // import paths from "./config/paths";
 // import Search from "./pages/Search";
 // import SignIn from "./pages/SignIn";
 // import SignUp from "./pages/SignUp";
 
-
 const curatorialHistory = createBrowserHistory();
 
 ReactDOM.render(
-    <Router onUpdate={()=> window.scrollTo(0, 0)} history={curatorialHistory} >
-     <ErrorBoundary>
-        <Route path="/" component={routerProps=> <App {...routerProps} />} />
+  <Router history={curatorialHistory}>
+    <ErrorBoundary>
+      <Route path="/" component={routerProps => <App {...routerProps} />} />
     </ErrorBoundary>
-    </Router>, 
-document.getElementById('root'));
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
