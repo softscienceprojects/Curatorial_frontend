2dc83b2abbe42df663f2c29e25c349934025e1a2
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-12 11:44:55 +0000

    span tags are now links

--- src/components/ArtworkCard.js
+++ src/components/ArtworkCard.js
@@ -1,33 +1,61 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import searchforArt from "../config/searchForArt";
 
-const ArtworkCard = (props) => {
+const ArtworkCard = props => {
+  return (
+    <div className="artwork-card">
+      <hr />
+      {/* Image below:  */}
+      <Link
+        to={{ pathname: `/explore/${props.artwork.id}` }}
+        key={props.artwork.id}
+      >
+        <img
+          src={props.artwork.image_url}
+          alt={props.artwork.title}
+          className="responsive"
+        />
+      </Link>
 
+       <div className="art-tags">
+      {/*  {props.searchTerm
+          ? props.artwork.content_names.map(content =>
+              content !== props.searchTerm ? (
+                <label className="tag">&nbsp;{content}&nbsp;</label>
+              ) : (
+                <label className="tag-selected">&nbsp;{content}&nbsp;</label>
+              )
+            )
+          : props.artwork.content_names.map(content => (
+              <label className="tag">&nbsp;{content}&nbsp;</label>
+            ))} */}
 
-    return(
-    
-        <div className="artwork-card">
-            <hr />
-        {/* Image below:  */}
-        <Link to={{pathname: `/explore/${props.artwork.id}`}} key={props.artwork.id}><img src={props.artwork.image_url} alt={props.artwork.title}  className="responsive" /></Link>
-        
-        <div className="art-tags">
-            {props.searchTerm ? 
-            props.artwork.contents.map(content=> 
-            content.description !== props.searchTerm
-            ? <label className="tag">&nbsp;{content.description}&nbsp;</label>  
-            : <label className="tag-selected">&nbsp;{content.description}&nbsp;</label>   
-            ) :
-            props.artwork.contents.map(content=>  
-            <label className="tag">&nbsp;{content.description}&nbsp;</label>  
-            )}  
-        </div>
+{props.searchTerm
+          ? props.artwork.content_names.map(content =>
+              content !== props.searchTerm ? (
+                <button onClick={() => searchforArt(content, props)} className="tag">&nbsp;{content}&nbsp;</button>
+              ) : (
+                <button onClick={() => searchforArt(content, props)} className="tag-selected">&nbsp;{content}&nbsp;</button>
+              )
+            )
+          : props.artwork.content_names.map(content => (
+              <button onClick={() => searchforArt(content, props)} className="tag">&nbsp;{content}&nbsp;</button>
+            ))}
+      </div>
 
-        <h4><Link to={{pathname: `/explore/${props.artwork.id}`}} key={props.artwork.id}>{props.artwork.title}</Link></h4>
-        <p>By {props.artwork.artist}</p>
-        <p>{props.artwork.medium}</p>
-        </div>
-    )
-}
+      <h4>
+        <Link
+          to={{ pathname: `/explore/${props.artwork.id}` }}
+          key={props.artwork.id}
+        >
+          {props.artwork.title}
+        </Link>
+      </h4>
+      <p>By {props.artwork.artist}</p>
+      <p>{props.artwork.medium}</p>
+    </div>
+  );
+};
 
-export default ArtworkCard
+export default ArtworkCard;
