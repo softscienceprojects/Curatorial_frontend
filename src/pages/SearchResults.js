211a86f92b3a9430228723a01bf67df9b36bcbce
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-12 12:04:41 +0000

    search query added to top of results

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -9,7 +9,7 @@ class SearchResults extends React.Component {
     render() {
         return(
             <article>
-                
+                <h1 id="logo">Results for {this.props.match.params.id}</h1>
                 {!this.props.location ? <LoadingComponent /> : null }
                 {this.props.location && this.props.location.state.errors ? <NoResults searchTerm={this.props.match.params.id} /> : null }
                 {this.props.location && this.props.location.state.content ? this.props.location.state.content.artworks.map(artwork=> <ArtworkCard {...this.props} searchTerm={this.props.match.params.id} key={`${artwork.id}-${artwork.title}`} artwork={artwork}/> ) : null }
